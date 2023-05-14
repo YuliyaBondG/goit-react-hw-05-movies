ddd33c1bf10461c4546613866d564648f3ef49c0
YuliyaBondG <manyunka@ukr.net>
2023-05-14 19:10:42 +0600

    add Lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,32 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from 'pages/Home/Home';
-import Movies from 'pages/Movies/Movies';
-import MoviesDetails from 'pages/MoviesDetail/MoviesDetails';
-import { Cast } from './Cast/Cast';
-import { Reviews } from './Reviews/Reviews';
-
+//import Home from 'pages/Home/Home';
+//import Movies from 'pages/Movies/Movies';
+//import MoviesDetails from 'pages/MoviesDetail/MoviesDetails';
+//import { Cast } from './Cast/Cast';
+//import { Reviews } from './Reviews/Reviews';
 import { SharedLayout } from './SharedLayout/SharedLayout';
-// import { lazy } from 'react';
+import { lazy } from 'react';
+const Home = lazy(() => import('../pages/Home/Home'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MoviesDetails = lazy(() => import('../pages/MoviesDetail/MoviesDetails'));
+const Cast = lazy(() =>
+  import('./Cast/Cast').then(module => {
+    console.log(module);
+    return {
+      ...module,
+      default: module.Cast,
+    };
+  })
+);
+const Reviews = lazy(() =>
+  import('./Reviews/Reviews').then(module => {
+    console.log(module);
+    return {
+      ...module,
+      default: module.Reviews,
+    };
+  })
+);
 
 export const App = () => {
   return (
